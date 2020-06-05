c7003899fdfa41620db3d28aea624b4500f54570
rakshaan06 <rakkymak@icloud.com>
2020-06-05 18:17:33 +0530

    i added a new class called Chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain, constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constrainedLog = new Log(230,180,80,PI/2);
+
+    chain = new Chain(bird.body,constrainedLog.body);
+
 }
 
 function draw(){
@@ -58,6 +64,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    constrainedLog.display();
+
+    chain.display();
 
     bird.display();
     platform.display();
